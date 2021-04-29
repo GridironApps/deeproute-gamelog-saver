ede74be88157ba5e18e509bdd9afb98d222e2f82
Garrett Foster <garrett.d.foster@gmail.com>
2021-04-28 21:56:59 -0500

    started adding player and player id data

--- utility.js
+++ utility.js
@@ -46,19 +46,41 @@ function parse_log(log_table) {
       off_formation = off[1].split(',')[0].trim();
       off_play = off[2].trim();
 
+      //offensive player ids
+      qb = ($rows.find('td:contains("QB ")').length > 0) ? $rows.find('td:contains("QB ")').eq(0).html().split('QB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      hb = ($rows.find('td:contains("HB ")').length > 0) ? $rows.find('td:contains("HB ")').eq(0).html().split('HB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      fb = ($rows.find('td:contains("FB ")').length > 0) ? $rows.find('td:contains("FB ")').eq(0).html().split('FB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      te1 = ($rows.find('td:contains("TE1 ")').length > 0) ? $rows.find('td:contains("TE1 ")').eq(0).html().split('TE1 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      te2 = ($rows.find('td:contains("TE2 ")').length > 0) ? $rows.find('td:contains("TE2 ")').eq(0).html().split('TE2 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wr1 = ($rows.find('td:contains("WR1 ")').length > 0) ? $rows.find('td:contains("WR1 ")').eq(0).html().split('WR1 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wr2 = ($rows.find('td:contains("WR2 ")').length > 0) ? $rows.find('td:contains("WR2 ")').eq(0).html().split('WR2 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wr3 = ($rows.find('td:contains("WR3 ")').length > 0) ? $rows.find('td:contains("WR3 ")').eq(0).html().split('WR3 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wr4 = ($rows.find('td:contains("WR4 ")').length > 0) ? $rows.find('td:contains("WR4 ")').eq(0).html().split('WR4 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wr5 = ($rows.find('td:contains("WR5 ")').length > 0) ? $rows.find('td:contains("WR5 ")').eq(0).html().split('WR5 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      lt = ($rows.find('td:contains("LT ")').length > 0) ? $rows.find('td:contains("LT ")').eq(0).html().split('LT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      lg = ($rows.find('td:contains("LG ")').length > 0) ? $rows.find('td:contains("LG ")').eq(0).html().split('LG ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      oc = ($rows.find('td:contains(" C ")').length > 0) ? $rows.find('td:contains(" C ")').eq(0).html().split(' C ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      rg = ($rows.find('td:contains("RG ")').length > 0) ? $rows.find('td:contains("RG ")').eq(0).html().split('RG ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      rt = ($rows.find('td:contains("RT ")').length > 0) ? $rows.find('td:contains("RT ")').eq(0).html().split('RT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+
       //reset variable values for new play
+      runner = '';
+      hole = '';
+      run_type = '';
       pass_type = '';
       pass_result = '';
       pass_direction = '';
       first_read = '';
       first_target = '';
+      good_coverage = '';
       final_target = '';
+      covered_by = '';
+      pass_defended = '';
       pressure_type = '';
+      sacked_by = '';
+      sack_allowed_by = '';
       pass_yards = '';
-      yac = '';
-      runner = '';
-      hole = '';
-      run_type = '';
+      yac = '';     
       is_touchdown = 0;
 
       //defensive playcall
@@ -76,6 +98,31 @@ function parse_log(log_table) {
       // break down coverage ??
       def_blitz = (def.split('Blitzing :')[1] ? def.split('Blitzing :')[1].trim().replace(/, /g, '+') : 'none');
 
+      //defensive player ids
+      lde = ($rows.find('td:contains("LDE ")').length > 0) ? $rows.find('td:contains("LDE ")').eq(0).html().split('LDE ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      ldt = ($rows.find('td:contains("LDT ")').length > 0) ? $rows.find('td:contains("LDT ")').eq(0).html().split('LDT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      lidt = ($rows.find('td:contains("LIDT ")').length > 0) ? $rows.find('td:contains("LIDT ")').eq(0).html().split('LIDT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      nt = ($rows.find('td:contains("NT ")').length > 0) ? $rows.find('td:contains("NT ")').eq(0).html().split('NT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      ridt = ($rows.find('td:contains("RIDT ")').length > 0) ? $rows.find('td:contains("RIDT ")').eq(0).html().split('RIDT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      rdt = ($rows.find('td:contains("RDT ")').length > 0) ? $rows.find('td:contains("RDT ")').eq(0).html().split('RDT ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      rde = ($rows.find('td:contains("RDE ")').length > 0) ? $rows.find('td:contains("RDE ")').eq(0).html().split('RDE ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wolb = ($rows.find('td:contains("WOLB ")').length > 0) ? $rows.find('td:contains("WOLB ")').eq(0).html().split('WOLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wlb = ($rows.find('td:contains("WLB ")').length > 0) ? $rows.find('td:contains("WLB ")').eq(0).html().split('WLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      wilb = ($rows.find('td:contains("WILB ")').length > 0) ? $rows.find('td:contains("WILB ")').eq(0).html().split('WILB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      mlb = ($rows.find('td:contains("MLB ")').length > 0) ? $rows.find('td:contains("MLB ")').eq(0).html().split('MLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      silb = ($rows.find('td:contains("SILB ")').length > 0) ? $rows.find('td:contains("SILB ")').eq(0).html().split('SILB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      slb = ($rows.find('td:contains("SLB ")').length > 0) ? $rows.find('td:contains("SLB ")').eq(0).html().split('SLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      solb = ($rows.find('td:contains("SOLB ")').length > 0) ? $rows.find('td:contains("SOLB ")').eq(0).html().split('SOLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      xlb = ($rows.find('td:contains("XLB ")').length > 0) ? $rows.find('td:contains("XLB ")').eq(0).html().split('XLB ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      c1 = ($rows.find('td:contains("C1 ")').length > 0) ? $rows.find('td:contains("C1 ")').eq(0).html().split('C1 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      c2 = ($rows.find('td:contains("C2 ")').length > 0) ? $rows.find('td:contains("C2 ")').eq(0).html().split('C2 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      n1 = ($rows.find('td:contains("N1 ")').length > 0) ? $rows.find('td:contains("N1 ")').eq(0).html().split('N1 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      n2 = ($rows.find('td:contains("N2 ")').length > 0) ? $rows.find('td:contains("N2 ")').eq(0).html().split('N2 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      n3 = ($rows.find('td:contains("N3 ")').length > 0) ? $rows.find('td:contains("N3 ")').eq(0).html().split('N3 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      n4 = ($rows.find('td:contains("N4 ")').length > 0) ? $rows.find('td:contains("N4 ")').eq(0).html().split('N4 ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      fs = ($rows.find('td:contains("FS ")').length > 0) ? $rows.find('td:contains("FS ")').eq(0).html().split('FS ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      ss = ($rows.find('td:contains("SS ")').length > 0) ? $rows.find('td:contains("SS ")').eq(0).html().split('SS ')[1].split('lookatplayer=')[1].split('&')[0] : '';
+      
       // check for offensive touchdowns
       if ($rows.find('td:contains("Touchdown")').length > 0) {
         is_touchdown = 1;
@@ -176,8 +223,22 @@ function parse_log(log_table) {
           first_read = 'open';
         }
 
+        //good coverage
+        if($rows.find('td:contains("Good coverage by ")').length > 0){
+          good_coverage = $rows.find('td:contains("Good coverage by ")').text().split('Good coverage by ')[1].split(' ')[0];
+        }
+
+        //pass defended
+        if($rows.find('td:contains("pass defended")').length > 0){
+          pass_defended = $rows.find('td:contains("pass defended")').text().split('credit ')[1].split(' ')[0];
+        }
+
+        //covering ... the targeted player
+        if($rows.find('td:contains("was the man covering on the play")').length > 0){
+          covered_by = $rows.find('td:contains("was the man covering on the play")').text().split(') - ')[1].split(' ')[0];
+        }
+
         // pressure
-        pressure_type = '';
         if (pass_type == 'scramble' || pass_type == "sack" || pass_type == "dump off") {
           if ($rows.find('td:contains(" under pressure from the Right side ")').length > 0) {
             pressure_type = 'pressure right';
@@ -186,6 +247,19 @@ function parse_log(log_table) {
           } else if ($rows.find('td:contains(" doesn\'t see anyone open ")').length > 0) {
             pressure_type = 'coverage';
           }
+        }
+
+        //get player to credit sack
+        if($rows.find('td:contains("SACKED")').length > 0){
+          sacked_by = $rows.find('td:contains("SACKED")').text().split(' SACKED by ')[1].split(' ')[0];
+        }
+
+        //get player to credit sack allowed
+        if($rows.find('td:contains("is charged with allowing the sack")').length > 0){ //TODO there are different message that trigget this, we could differentiate
+          sack_allowed_by = $rows.find('td:contains("is charged with allowing the sack")').text().split('yard loss.  ')[1].split(' ')[0];
+        }
+        if($rows.find('td:contains(" is responsible for allowing the sack")').length > 0){ 
+          sack_allowed_by = $rows.find('td:contains(" is responsible for allowing the sack")').text().split('Yard(s). ')[1].split(' ')[0];
         }
 
         //targets
@@ -286,10 +360,48 @@ function parse_log(log_table) {
         off_team: off_team,
         off_package: off_pkg,
         off_formation: off_formation,
+        QB: qb,
+        HB: hb,
+        FB: fb,
+        TE1: te1,
+        TE2: te2,
+        WR1: wr1,
+        WR2: wr2,
+        WR3: wr3,
+        WR4: wr4,
+        WR5: wr5,
+        LT: lt,
+        LG: lg,
+        C: oc,
+        RG: rg,
+        RT: rt, 
         off_play: off_play,
         play_type: play_type,
         def_team: def_team,
         def_package: def_pkg,
+        LDE: lde,
+        LDT: ldt,
+        LIDT: lidt,
+        NT: nt,
+        RIDT: ridt,
+        RDT: rdt,
+        RDT: rde,
+        WOLB: wolb,
+        WLB: wlb,
+        WILB: wilb,
+        MLB: mlb,
+        SILB: silb,
+        SLB: slb,
+        SOLB: solb,
+        XLB: xlb,
+        C1: c1,
+        C2: c2,
+        N1: n1,
+        N2: n2,
+        N3: n3,
+        N4: n4,
+        FS: fs,
+        SS: ss,
         cover_type: cvr_type,
         cover_depth: cvr_depth,
         roamer_job: roamer_job,
@@ -303,8 +415,13 @@ function parse_log(log_table) {
         pass_direction: pass_direction,
         first_read: first_read,
         first_target: first_target,
+        good_coverage: good_coverage,
         final_target: final_target,
+        covered_by: covered_by,
+        pass_defended: pass_defended,
         pressure_type: pressure_type,
+        sacked_by: sacked_by,
+        sack_allowed_by: sack_allowed_by,
         target_distance: pass_yards,
         yards_after_catch: yac,
         is_touchdown: is_touchdown
